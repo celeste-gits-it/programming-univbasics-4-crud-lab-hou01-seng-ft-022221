aea219d2c8d8e9a2c966c39bb2cb9014100c540a
Celeste Carter <celesteanell@gmail.com>
2021-02-08 18:23:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array = ["evianna", "evonna"]
-  add_element_to_end_of_array.push ("tasheka")
+  add_element_to_end_of_array.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
