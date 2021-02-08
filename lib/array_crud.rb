52f60fb9ac61eecb339e9a0067411d9c59b974eb
Celeste Carter <celesteanell@gmail.com>
2021-02-08 18:15:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+   []
 end
 
 def create_an_array
-  
+  ["omar", "celeste", "evianna", "evonna"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array = ("omar", "celeste", "evianna", "evonna")
+  add_element_to_end_of_array << ("tasheka")
 end
 
 def add_element_to_start_of_array(array, element)
